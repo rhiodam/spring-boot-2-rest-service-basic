36265332f89aafd893528a3270274cc088515a21
rhiodam <rhioda.m@gmail.com>
2019-02-21 22:25:38 +0700

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into student
+values(10001,'Ranga', 'E1234567');
+
+insert into student
+values(10002,'Ravi', 'A1234568');
+
+insert into student
+values(10003,'Rangasdfsdfsd', 'E1234567');
+
+insert into student
+values(10004,'Ravisdfsdfsdfsdfsdfsdfsdf', 'A1234568');
